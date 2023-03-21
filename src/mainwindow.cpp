34e3dfca080fbab41288163dec03fac5b8cb0224
WaperDaBapper <Elias.Sundberg98@outlook.com>
2023-03-21 02:13:50 +0100

    Mainwindow
    Styling and other ideas

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QPixmap>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QPixmap pix("..\\imgs\\LeikaMiniBanner.png"); // Default logo for now, later change it to custom for servers
+    pix.scaled(100, 100, Qt::KeepAspectRatio);
+    ui->banner->setPixmap(pix);
+
+    //ui->label_5->setText("");
 }
 
 MainWindow::~MainWindow()
